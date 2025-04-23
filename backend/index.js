7434e0d36c695181ee06b54a13c34e0f0890a012
Manish Kumar <ms5798034@gmail.com>
2025-04-23 21:41:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,9 @@ app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/",(req,res)=>{
+    res.send("Welcome to the API");
+});
 
 // api's
 app.use("/api/v1/user", userRoute);
